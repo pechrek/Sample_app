c99da517b2cee661599a40a7e46aedbc63b6caab
Artem Morozov <artmorozov@gmail.com>
2011-12-01 16:36:09 +0400

    Finnished layots and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SampleApp::Application.routes.draw do
-  match '/contact', to => 'pages#contact'
+  get "users/new"
 
-  match '/about', to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+
+  match '/about', :to => 'pages#about'
   
-  match '/help', to => 'pages#help'
+  match '/help', :to => 'pages#help'
+  
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
